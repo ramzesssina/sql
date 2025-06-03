046fc7d67f74bf79f1bff5108e497f90ea93f7cc
Ramzes <133229713+ramzesssina@users.noreply.github.com>
2025-06-03 21:46:37 +1000

    Add files via upload

---
+++ officedb.sql
@@ -0,0 +1,82 @@
+create database office default character set = 'utf8';
+show create database office;
+use office;
+
+create table department (
+	id int auto_increment key,
+	`name` varchar(200) not null unique)
+;
+
+create table employee (
+	id int auto_increment key,
+	`name` varchar(100),
+	salary int)
+;
+
+alter table employee
+	add column department_id int default null,
+	add foreign key (department_id) references department(id),
+	add column chief_id int default null,
+	add foreign key (chief_id) references employee(id)
+;
+
+insert into department(`name`) values
+	("Отдел продаж"),
+	("Отдел технической поддержки"),
+	("Бухгалтерия"),
+	("Отдел маркетинга"),
+	("Отдел кадров")
+;
+
+insert into employee(department_id, chief_id, `name`, salary) values
+	(1, null, "Белов Владимир Александрович", 150000),
+	(1, 1,"Федорова Полина Дмитриевна", 130000),
+	(1, 1,"Комаров Андрей Егорович", 134567),
+	(2, null, "Степанов Владимир Павлович", 201123),
+	(2, 4, "Ермолаев Тимофей Романович", 156040),
+	(3, null, "Козырев Кирилл Станиславович", 175938),
+	(3, 6, "Анохин Арсений Николаевич", 208400),
+	(3, 6, "Фролова Анастасия Руслановна", 172300),
+	(3, 6, "Ковалева Вера Романовна", 104500),
+	(3, 6, "Коновалова Диана Артёмовна", 91870),
+	(4, null, "Герасимова Александра Александровна", 200156),
+	(4, null, "Филатова Татьяна Никитична", 131430),
+	(5, null, "Федосеева Анастасия Алексеевна", 120432),
+	(5, 13, "Фадеева Алиса Евгеньевна", 95058),
+	(5, 13, "Карпов Марк Маркович", 87940)
+;
+
+select * from employee;
+
+#Задание 1
+select e.`name`, e.salary 
+from employee e, employee em
+where em.id = e.chief_id and e.salary > em.salary;
+
+#Zadanie 2
+select e.`name`, e.salary, d.`name`
+from employee e join department d on e.department_id = d.id
+where e.salary = ( select MAX(salary) from employee em where em.department_id = e.department_id )
+;
+
+#Zadanie 3
+select department_id, COUNT(id) 
+from employee 
+group by department_id having count(id) <= 3
+;
+
+#Zadanie 4
+select e.name, e.salary, d.name AS department_name
+from employee e
+join department d on e.department_id = d.id
+where e.chief_id is null 
+and not exists (
+    select * from employee e2 
+    where e2.department_id = e.department_id 
+    and e2.chief_id is not null
+);
+
+#Zadanie 5
+select department_id, sum(salary) salary
+from employee group by department_id having sum(salary) = (select sum(salary) from employee group by department_id order by sum(salary) desc limit 1)
+;
