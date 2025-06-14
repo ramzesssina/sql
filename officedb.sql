fa91f7bf4068d44edc652c5b7a49023b07d0e366
Ramzes <133229713+ramzesssina@users.noreply.github.com>
2025-06-14 22:10:30 +1000

    Add files via upload

--- officedb.sql
+++ officedb.sql
@@ -25,25 +25,34 @@ insert into department(`name`) values
 	("Отдел технической поддержки"),
 	("Бухгалтерия"),
 	("Отдел маркетинга"),
-	("Отдел кадров")
+	("Отдел кадров"),
+    ("Отдел рекламы")
 ;
 
 insert into employee(department_id, chief_id, `name`, salary) values
 	(1, null, "Белов Владимир Александрович", 150000),
 	(1, 1,"Федорова Полина Дмитриевна", 130000),
 	(1, 1,"Комаров Андрей Егорович", 134567),
+    
 	(2, null, "Степанов Владимир Павлович", 201123),
 	(2, 4, "Ермолаев Тимофей Романович", 156040),
+    
 	(3, null, "Козырев Кирилл Станиславович", 175938),
 	(3, 6, "Анохин Арсений Николаевич", 208400),
 	(3, 6, "Фролова Анастасия Руслановна", 172300),
 	(3, 6, "Ковалева Вера Романовна", 104500),
 	(3, 6, "Коновалова Диана Артёмовна", 91870),
+    
 	(4, null, "Герасимова Александра Александровна", 200156),
-	(4, null, "Филатова Татьяна Никитична", 131430),
+	(4, 11, "Филатова Татьяна Никитична", 131430),
+    
 	(5, null, "Федосеева Анастасия Алексеевна", 120432),
-	(5, 13, "Фадеева Алиса Евгеньевна", 95058),
-	(5, 13, "Карпов Марк Маркович", 87940)
+	(5, 13, "Фадеева Алиса Евгеньевна", 130433),
+	(5, 13, "Карпов Марк Маркович", 87940),
+    
+    (6, 11, "Зайцев Андрей Романович", 175907),
+	(6, 11, "Дегтярева Анна Дамировна", 95026),
+	(6, 11, "Орлова Мария Фёдоровна", 93112)
 ;
 
 select * from employee;
@@ -66,17 +75,16 @@ group by department_id having count(id) <= 3
 ;
 
 #Zadanie 4
-select e.name, e.salary, d.name AS department_name
+select e.name as 'имя сотрудника', e.salary as 'Зарплата', d.name as 'название отдела', chief.name as 'имя шефа', chief_dep.name as 'название отдела' 
 from employee e
 join department d on e.department_id = d.id
-where e.chief_id is null 
-and not exists (
-    select * from employee e2 
-    where e2.department_id = e.department_id 
-    and e2.chief_id is not null
-);
+join employee chief on e.chief_id = chief.id
+join department chief_dep on chief.department_id = chief_dep.id
+where e.department_id != chief.department_id;# Руководитель в другом отделе
+
+select * from employee;
 
 #Zadanie 5
 select department_id, sum(salary) salary
-from employee group by department_id having sum(salary) = (select sum(salary) from employee group by department_id order by sum(salary) desc limit 1)
-;
+from employee group by department_id having 
+sum(salary) = (select sum(salary) from employee group by department_id order by sum(salary) desc limit 1);
